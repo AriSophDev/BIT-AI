ca7693c18835856c2a8b85423d18de3624c27cdb
Arian Sophia <111167233+AriSophDev@users.noreply.github.com>
2024-08-28 06:55:27 -0500

    add welcome view

---
+++ Chat IA/Chat IA/SubViews/WelcomeView.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeView.swift
+//  Chat IA
+//
+//  Created by Arian Large  on 28/08/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Image("logo")
+            Text("Bienvenido a Bit AI")
+                .padding()
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Button("Iniciar Sesión")
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
